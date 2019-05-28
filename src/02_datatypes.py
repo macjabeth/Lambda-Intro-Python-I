0dc66bbf436085923ad53d8f7914f6d5bbfef81a
Jonathan Picazo <macbethjonathan@gmail.com>
2019-05-28 14:19:13 -0400

    complete datatypes challenge

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -1,7 +1,7 @@
 """
-Python is a strongly-typed language under the hood, which means 
+Python is a strongly-typed language under the hood, which means
 that the types of values matter, especially when we're trying
-to perform operations on them. 
+to perform operations on them.
 
 Note that if you try running the following code without making any
 changes, you'll get a TypeError saying you can't perform an operation
@@ -11,11 +11,14 @@ on a string and an integer.
 x = 5
 y = "7"
 
+# Display the values we're working with
+
+print('x:', x, 'y:', y)
+
 # Write a print statement that combines x + y into the integer value 12
 
-# YOUR CODE HERE
-
+print('combine `x + y` to integer:', x + int(y))
 
 # Write a print statement that combines x + y into the string value 57
 
-# YOUR CODE HERE
+print('combine `x + y` to string:', str(x) + y)
