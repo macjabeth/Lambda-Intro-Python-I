5ebb802d67bfade93b5117c49274c45563a2baf9
Jonathan Picazo <macbethjonathan@gmail.com>
2019-05-29 02:15:38 -0400

    complete file challenge

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -1,18 +1,24 @@
 """
 Python makes performing file I/O simple. Take a look
-at how to read and write to files here: 
+at how to read and write to files here:
 
 https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 """
 
+import os
+
 # Open up the "foo.txt" file (which already exists) for reading
 # Print all the contents of the file, then close the file
 
-# YOUR CODE HERE
+# this was torture - there must be an easier way (relative path wasn't working)
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'foo.txt')) as f:
+  read_data = f.read()
+  print(read_data)
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
-# then close the file. Open up "bar.txt" and inspect it to make 
+# then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
-# YOUR CODE HERE
+with open('bar.txt', 'w') as f:
+  f.write('one\ntwo\nthree\n')
